cd1a476690666ce546e41c987f5f680545dc9392
MarielML <marielmarmol1511@outlook.com>
2023-04-22 22:22:44 -0300

    cambios

--- app/src/main/java/ar/edu/unlam/mobile2/ui/JuegoActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/ui/JuegoActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.ui.AppBarConfiguration
 import ar.edu.unlam.mobile2.R
 import ar.edu.unlam.mobile2.databinding.ActivityMainBinding
@@ -30,6 +34,9 @@ class JuegoActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var puntaje = 0
+
         setContent {
             Mobile2_ScaffoldingTheme {
                 // A surface container using the 'background' color from the theme
@@ -52,6 +59,7 @@ class JuegoActivity : ComponentActivity() {
                             model = mainViewModel.getImageRequest(LocalContext.current),
                             contentDescription = stringResource(R.string.anime_image2)
                         )
+                        Text(puntaje.toString(), Modifier.background(Color.Red, RectangleShape).padding(10.dp), Color.White)
                     }
                 }
             }
