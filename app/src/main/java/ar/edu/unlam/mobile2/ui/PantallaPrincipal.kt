d657d7ece1b7ae0d45b5e14169e43d08a8abd2e6
FrancoMazaffra <francomazaffra@hotmail.com>
2023-04-25 23:57:46 -0300

    se agrega pantalla de qr y navegaciición

--- app/src/main/java/ar/edu/unlam/mobile2/ui/PantallaPrincipal.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/ui/PantallaPrincipal.kt
@@ -1,6 +1,7 @@
 package ar.edu.unlam.mobile2.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 
 import androidx.compose.foundation.background
 import android.os.Bundle
@@ -27,15 +28,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.smallTopAppBarColors
-import androidx.compose.material3.rememberTopAppBarState
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -46,7 +43,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 
 import androidx.compose.ui.res.painterResource
 
@@ -167,10 +163,10 @@ pantallaInicio()
             colors = ButtonDefaults.buttonColors( Color(0xFF396AE9)),
 
 
-            onClick = {/*
-                startActivity(Intent(this@PantallaPrincipal,PantallaPrincipalVersus::class.java))
+            onClick = {
+                startActivity(Intent(this@PantallaPrincipal, PantallaVersus::class.java))
                 finish()
-            */}) {
+            }) {
             Text(text = "Versus")
 
         }
