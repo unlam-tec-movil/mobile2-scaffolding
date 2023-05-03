7d9ae27cd8eff85f39f8c0018622242d64b0da51
Fabischaffts <124001856+Fabischaffts@users.noreply.github.com>
2023-05-02 21:05:59 -0300

    appcenter

--- app/src/main/java/ar/edu/unlam/mobile2/MainModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/MainModule.kt
@@ -6,27 +6,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-/*import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes*/
-<<<<<<< Updated upstream
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class MainModule {
-   /* override fun onCreate(savedInstanceState: Bundle?) {
-=======
-//en main Activity
-@Module
-@InstallIn(ViewModelComponent::class)
-abstract class MainModule {
-    /*  override fun onCreate(savedInstanceState: Bundle?) {
->>>>>>> Stashed changes
-        super.onCreate(savedInstanceState)
-        AppCenter.start(
-            application, "719d2c40-5de1-44d8-980d-aded581ac26d",
-            Analytics::class.java, Crashes::class.java
-        )*/
 
     @Binds
     abstract fun bindKittiesRepo(kittiesRestRepo: KittiesRestRepo): KittiesRepo
---
+++ app/src/main/java/ar/edu/unlam/mobile2/domain/MainActivity.kt
@@ -0,0 +1,54 @@
+package ar.edu.unlam.mobile2.domain
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ar.edu.unlam.mobile2.domain.ui.theme.Mobile2_ScaffoldingTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "719d2c40-5de1-44d8-980d-aded581ac26d",
+            Analytics::class.java, Crashes::class.java
+        )
+        setContent {
+            Mobile2_ScaffoldingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Mobile2_ScaffoldingTheme {
+        Greeting("Android")
+    }
+}
