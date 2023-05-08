5f011fd08b748ea85569ace5d56fd15cd1960164
MarielML <marielmarmol1511@outlook.com>
2023-05-08 12:04:52 -0300

    cambios

--- app/src/main/java/ar/edu/unlam/mobile2/ui/RecordActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/ui/RecordActivity.kt
@@ -1,29 +1,34 @@
 package ar.edu.unlam.mobile2.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ar.edu.unlam.mobile2.R
 import ar.edu.unlam.mobile2.ui.ui.theme.Mobile2_ScaffoldingTheme
 import ar.edu.unlam.mobile2.ui.ui.theme.OrangeLight
 import ar.edu.unlam.mobile2.ui.ui.theme.VioletDark
@@ -48,11 +53,32 @@ class RecordActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 )
                 {
+                    TopBar()
+                    Spacer(Modifier.size(5.dp))
                     Ranking()
                     Spacer(Modifier.size(30.dp))
-                    BotonVolver()
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun TopBar() {
+        Row(
+            modifier = Modifier
+                .background(VioletDark)
+                .fillMaxWidth()
+                .height(36.dp)
+                .wrapContentHeight()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_close_24),
+                contentDescription = "",
+                modifier = Modifier.clickable(enabled = true, onClick = {
+                    irAInicio()
+                    onStop()
+                })
+            )
         }
     }
 
@@ -69,88 +95,7 @@ class RecordActivity : ComponentActivity() {
             Text(text = "Puntaje", color = Color.White, fontSize = 30.sp)
         }
         Spacer(Modifier.size(10.dp))
-        MostrarRanking(ListaDeJuegosPrueba.juegos)
-        /*Row (
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "1")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "2")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "3")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "4")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "5")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "6")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "7")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "8")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "9")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }
-        Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Texto(texto = "10")
-            Texto(texto = "Jugador")
-            Texto(texto = "100")
-        }*/
+        MostrarRanking(ListaDeJuegosPrueba.ordenar() as MutableList<JuegoPrueba>)
     }
 
     @Composable
@@ -177,30 +122,11 @@ class RecordActivity : ComponentActivity() {
             Texto(item.jugador)
             Texto(item.puntaje.toString())
         }
-
-        /*Row (modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Row {
-                Text(item.jugador)
-                Text(item.puntaje.toString())
-            }
-
-        }*/
     }
 
-    @Composable
-    private fun BotonVolver() {
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(onClick = { finish()}, contentPadding = PaddingValues(10.dp)) {
-                Text(text = "VOLVER", fontSize = 30.sp)
-            }
-        }
+    private fun irAInicio() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
 
@@ -218,12 +144,23 @@ object ListaDeJuegosPrueba {
         agregar(JuegoPrueba("Jugador 3", 70))
         agregar(JuegoPrueba("Jugador 4", 25))
         agregar(JuegoPrueba("Jugador 5", 30))
+        agregar(JuegoPrueba("Jugador 6", 10))
+        agregar(JuegoPrueba("Jugador 7", 30))
+        agregar(JuegoPrueba("Jugador 8", 30))
+        agregar(JuegoPrueba("Jugador 9", 10))
+        agregar(JuegoPrueba("Jugador 10", 30))
+        agregar(JuegoPrueba("Jugador 11", 30))
+        agregar(JuegoPrueba("Jugador 12", 15))
+        agregar(JuegoPrueba("Jugador 13", 200))
+        agregar(JuegoPrueba("Jugador 14", 390))
+        agregar(JuegoPrueba("Jugador 15", 5))
     }
+
     fun agregar(juego: JuegoPrueba) {
         juegos.add(juego)
     }
 
-    fun ordenar() {
-        juegos.sortedByDescending { it.puntaje }
+    fun ordenar(): List<JuegoPrueba> {
+        return juegos.sortedByDescending { it.puntaje }
     }
 }
