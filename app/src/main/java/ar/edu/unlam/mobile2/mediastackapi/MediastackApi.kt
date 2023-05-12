a52e222cc1c716ebfa1f67ef35eec23ed784e667
Nahuel Palacio <palacionahuel202@gmail.com>
2023-05-12 20:08:31 -0300

    1
    1

--- app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/MediastackApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/MediastackApi.kt
@@ -6,8 +6,8 @@ import retrofit2.http.Query
 
 interface MediastackApi {
 
-    @GET("v1/news")
+    @GET("v1/news?access_key=33597a00a28ca90e3e5ac791b76b3e6e")
     suspend fun getNews(
-        @Query("access_key") apiKey: String = "33597a00a28ca90e3e5ac791b76b3e6e"
+        //@Query("access_key") apiKey: String = "33597a00a28ca90e3e5ac791b76b3e6e"
     ): NewsResponse
 }
