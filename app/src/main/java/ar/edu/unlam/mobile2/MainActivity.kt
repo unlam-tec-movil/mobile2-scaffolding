2e234d1bede0efcf4fa866e5a8f2c41605ad8fb0
Nahuel Palacio <palacionahuel202@gmail.com>
2023-05-21 17:53:33 -0300

    Correcciones

--- app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -44,8 +45,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WeatherScreen(weatherViewModel)
-                    MockScreen(viewModel)
+                    Column() {
+                        WeatherScreen(weatherViewModel)
+                        MockScreen(viewModel)
+                    }
+
                 }
             }
         }
