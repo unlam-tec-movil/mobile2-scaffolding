7706aaeb48a3340955d9a61dac62db8a165b4ee4
Nahuel Palacio <palacionahuel202@gmail.com>
2023-06-01 20:39:06 -0300

    livedata
    creacion de objeto mutablelivedata y observer

--- app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/ui/NewsList.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/ui/NewsList.kt
@@ -1,16 +1,15 @@
 package ar.edu.unlam.mobile2.mediastackapi.ui
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Observer
+import ar.edu.unlam.mobile2.mediastackapi.viewmodel.NewState
 import ar.edu.unlam.mobile2.mediastackapi.viewmodel.NewsViewModel
 import ar.edu.unlam.mobile2.ui.CardContainer
 
@@ -18,9 +17,11 @@ import ar.edu.unlam.mobile2.ui.CardContainer
 fun NewsList(viewModel: NewsViewModel){
     val state = viewModel.state
 
-    if (state.news.isNotEmpty()){
+    val stateLiveData = viewModel.stateLiveData.observeAsState()
+
+    if (stateLiveData.value?.isNotEmpty() == true){
         LazyColumn(modifier = Modifier.fillMaxWidth()){
-            items(state.news){
+            items(stateLiveData.value!!){
                 CardContainer(it)
             }
         }
--- app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/viewmodel/NewsViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/viewmodel/NewsViewModel.kt
@@ -1,11 +1,14 @@
 package ar.edu.unlam.mobile2.mediastackapi.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ar.edu.unlam.mobile2.mediastackapi.NewsRepository
+import ar.edu.unlam.mobile2.mediastackapi.data.Data
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,15 +21,17 @@ class NewsViewModel @Inject constructor(
     var state by mutableStateOf(NewState())
         private set
 
+    val stateLiveData: MutableLiveData<List<Data>> by lazy {
+        MutableLiveData<List<Data>>()
+    }
+
     init {
         viewModelScope.launch {
             repository.getNews().onSuccess {
-                state = state.copy(
-                    news = it
-                )
-                println("funca")
+                stateLiveData.value = it
+                Log.i("SUCCESS API", "Se recibieron datos de la api")
             }.onFailure {
-                println("uh")
+                Log.e("FAILURE API", "NO se recibieron datos de la api")
             }
         }
     }
