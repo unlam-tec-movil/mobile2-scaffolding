a6e7047630e60da37733a1d4f915acb73f1a79d3
Ezequiel Torres <e.torres.baquedano@gmail.com>
2023-06-06 22:24:53 -0300

    Se modifico el UI de Noticias y Clima

--- app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/ui/NewCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/mediastackapi/ui/NewCard.kt
@@ -1,13 +1,19 @@
 package ar.edu.unlam.mobile2.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,17 +27,29 @@ fun ArticleCard() {
 
 @Composable
 fun CardContainer(new: New) {
-    Column(
-        modifier = Modifier.padding(horizontal = 8.dp).padding(top = 5.dp)
-    ) {
-        Text(text = new.category!!)
-        Text(
-            text = new.title!!,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp
-        )
-        Spacer(modifier = Modifier.height(5.dp))
-        Divider()
+    Card(modifier = Modifier
+        .padding(1.dp)
+        .clickable { }
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp)) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(top = 5.dp)
+        ) {
+           Row() {
+               Text(text = "Categoria: ")
+               Text(text = new.category!!)
+           } 
+            Text(
+                text = new.title!!,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Divider()
+        }
     }
 }
 
--- app/src/main/java/ar/edu/unlam/mobile2/weatherapi/ui/ApiScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile2/weatherapi/ui/ApiScreen.kt
@@ -1,7 +1,17 @@
 package ar.edu.unlam.mobile2.weatherapi.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,37 +20,60 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import ar.edu.unlam.mobile2.R
 import ar.edu.unlam.mobile2.weatherapi.data.WeatherResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 @Composable
+
 fun WeatherScreen(weatherViewModel: WeatherViewModel) {
     var weatherData by remember { mutableStateOf<WeatherResponse?>(null) }
 
-    LaunchedEffect(Unit) {
+   LaunchedEffect(Unit) {
         val data = withContext(Dispatchers.IO) {
             weatherViewModel.getWeatherData("Buenos Aires")
         }
         weatherData = data
     }
+    
+   Card(modifier = Modifier
+       .padding(1.dp)
+       .clickable { }
+       .fillMaxWidth(),
+   shape = RoundedCornerShape(8.dp)) {
+       Column(
+           modifier = Modifier.padding(16.dp)
+       ) {
 
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        if (weatherData != null) {
-            Text(
-                text = "Temperature: ${weatherData!!.current.temperature}",
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
+        Image(painter = painterResource(
+               id = R.drawable.clima),
+               contentDescription = "Clima ",
+               modifier = Modifier
+                   .size(100.dp),
+               contentScale = ContentScale.Crop)
+               if (weatherData != null) {
+                   Text(
+                       text = "Temperatura: ${weatherData!!.current.temperature}",
+                       color = Color.White,
+                       modifier = Modifier.padding(bottom = 8.dp)
+                   )
 
-            Text(
-                text = "Weather Description: ${weatherData!!.current.weatherDescriptions.first()}",
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-        } else {
-            Text("Loading weather data...")
-        }
-    }
+                   Text(
+                       text = "Weather Description: ${weatherData!!.current.weatherDescriptions.first()}",
+                       color = Color.White,
+                       modifier = Modifier.padding(bottom = 8.dp)
+                   )
+               } else {
+                   Text("Loading weather data...")
+               }
+
+       }
+   }
+
+
 }
